69c374b3075cecbef1f82f50101de0294dc307dc
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-04-27 00:31:21 +0900

    create url routing

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import ItemView from '../views/ItemView.vue';
+import UserView from '../views/UserView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/news',
+        },
+        {
+            //url
+            path: '/news',
+            // component (page)
+            component: NewsView,
+        },
+        {
+            //url
+            path: '/jobs',
+            // component (page)
+            component: JobsView,
+        },
+        {
+            //url
+            path: '/ask',
+            // component (page)
+            component: AskView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+    ]
+});
